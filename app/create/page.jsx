dbaebcecdd2954355121a08bebdedad00131fe01
Krish <gujarathikrish@gmail.com>
2025-05-18 16:57:42 +0530

    third commit

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import React, { useState } from "react";
 import LogoTitle from "./_component/LogoTitle";
 import { Button } from "@/components/ui/button";
